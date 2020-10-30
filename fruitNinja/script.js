5c3c410a8e95e854ebc09a6bfafb8ffee623d811
RoboBeats <61983132+RoboBeats@users.noreply.github.com>
2020-10-30 16:43:28 +0530

    Add files via upload

--- fruitNinja/script.js
+++ fruitNinja/script.js
@@ -3,6 +3,14 @@ var melons;
 var fruitVelocity;
 var lives;
 var score;
+var slashed;
+
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
 
 class Melon {
     constructor() {
@@ -11,30 +19,77 @@ class Melon {
         this.width = 87.5;
         this.height = 70;
         this.speed = fruitVelocity;
-        this.left = this.x;
-        this.right = this.x + this.width;
-        this.top = this.y;
-        this.bottom = this.y + this.height;
+        this.points = this.getPoints();
         this.isFunc = true;
         this.image = new Image();
         this.image.src = "images/melon.png";
     }
+    getPoints() {
+        return [
+            [this.x, this.y],
+            [this.x + this.width, this.y],
+            [this.x + this.width, this.y + this.height],
+            [this.x, this.y + this.height],
+            [this.x, this.y]
+        ];
+    }
     draw(ctx) {
+        ctx.fillStyle = "aqua";
+        ctx.fillRect(this.x, this.y, this.width, this.height);
         ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
         this.width += 1.25/fruitVelocity;
         this.height += 1/fruitVelocity;
         this.x -= (1.25/fruitVelocity)/2;
         this.y -= (1/fruitVelocity)/2;
 
-        this.left = this.x;
-        this.right = this.x + this.width;
-        this.top = this.y;
-        this.bottom = this.y + this.height;
+        this.points = this.getPoints();
         if (this.width > 120 && this.height > 100) {
             //this.isFunc = false;
             //lives--;
         }
     }
+}
+
+function handleSlash(startX, startY, endX, endY, ctx) {
+    var startCoordinates = [startX, startY];
+    var endCoordinates = [endX, endY];
+    var slashEquation = getLineEquation(startCoordinates, endCoordinates);
+    for (var i = 0; i < melons.length; i++) {
+        var melon = melons[i];
+        var numInter = 0;
+        for (var j = 0; j < 4; j++) {
+            var fruitEquation = getLineEquation(melon.points[j], melon.points[j+1]);
+            var intPoint = getIntersection(fruitEquation, slashEquation);
+            ctx.fillStyle = "orange";
+            ctx.fillRect(intPoint[0], intPoint[1], 1, 1);
+            var pointX = intPoint[0];
+            var pointY = intPoint[1];
+            if (
+                (pointX >= melon.points[j][0] &&
+                pointX <= melon.points[j+1][0] &&
+                pointY >= melon.points[j][1] &&
+                pointY <= melon.points[j+1][1]) ||
+                (pointX <= melon.points[j][0] &&
+                pointX >= melon.points[j+1][0] &&
+                pointY <= melon.points[j][1] &&
+                pointY >= melon.points[j+1][1])
+            ) {
+                numInter++;
+                console.log(numInter);
+            }
+        }
+        if (numInter >= 2) {
+            melon.isFunc = false;
+            score++;
+        }
+    }
+}
+function getLineEquation(p1, p2) {
+    var m = (p2[1] - p1[0])/(p2[0] - p1[0]);
+    return [m, p1[1] - m*p1[0]];
+}
+function getIntersection(eq1, eq2) {
+    return [(eq1[1] - eq2[1])/(eq2[0]-eq1[0]), ((eq1[1]*eq2[0]) - (eq2[1]*eq1[0]))/(eq2[0] - eq1[0])];
 }
 
 function initGame() {
@@ -47,20 +102,9 @@ function initGame() {
     startY = 0;
     endX = canvasWidth;
     endY = canvasHeight;
-    slashed = true;
     score = 0;
+    slashed = false;
 }
-
-function handleSlash(startX, startY, endX, endY) {
-    for (var i = 0; i < melons.length; i++) {
-        if (startX <= melons[i].left && endX >= melons[i].right && startY <= melons[i].top && endY >= melons[i].bottom) {
-            console.log("melon eliminated");
-            melons.splice(i, 1);
-            score++;
-        }
-    }
-}
-
 function startGame(event) {
     var c = document.getElementById("myCanvas");
     var ctx = c.getContext("2d");
@@ -74,6 +118,12 @@ function startGame(event) {
         var background = new Image;
         background.src = "images/background.jpg";
         ctx.drawImage(background, 0, 0, canvasWidth, canvasHeight);
+        ctx.beginPath();
+        ctx.moveTo(0, 0);
+        ctx.lineTo(canvasWidth, canvasHeight);
+        ctx.stroke();
+
+        handleSlash(0, 0, canvasWidth, canvasHeight, ctx);
 
         for (var i = 0; i < melons.length; i++) {
             if (!melons[i].isFunc) {
